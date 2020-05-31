2016828861a9eff76225638665a933f3320ee9c4
Trisha :) <trigan141@fusdk12.net>
2020-05-31 02:59:06 +0000

    Initial commit

---
+++ database.py
@@ -0,0 +1,31 @@
+"""
+Concerned with storing and retrieving books from a list. 
+"""
+
+books = []
+
+
+def add_book(name, author):
+    books.append({'name': name, 'author': author, 'read': False})
+
+
+def get_all_books(): 
+    return books
+
+
+def mark_book_as_read(name): 
+    for book in books: 
+        if book['name'] == name:
+            book['read'] = True
+
+
+def delete_book(name): 
+    global books 
+    books = [book for book in books if book['name'] != name] 
+
+
+
+# def delete_book(name):
+#    for book in books: 
+#        if book['name'] == name: 
+#            book.remove(book)
